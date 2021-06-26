6e40cb2fcca46af66cabb49641f08263e03921d4
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2021-06-25 19:43:28 -0500

    Create view.js

---
+++ a_svg/js/view.js
@@ -0,0 +1,54 @@
+
+var svg = `<svg class="tierra">
+		<rect class="principal" />
+		<rect class="menu" />
+		<text class="tit_bien" x="133" y="70">¡Bienvenido!</text>
+		<text class="con_bien" x="100" y="100">----------------------------------</text>
+		<text class="con_bien" x="100" y="130">Utiliza el menú de la</text>
+		<text class="con_bien" x="100" y="150">izquierda para poder</text>
+		<text class="con_bien" x="100" y="170">conocer mi perfil.</text>
+		<text class="con_bien" x="100" y="235">----------------------------------</text>
+		<text class="con_bien" x="135" y="255">2018 - 2021</text>
+		
+		<text class="tit_rojo" x="100" y="70">Óscar González</text>
+		<text class="con_rojo" x="100" y="100">----------------------------------</text>
+		<text class="con_rojo" x="100" y="130">To. ADSI - SENA</text>
+		<text class="con_rojo" x="100" y="150">Análisis y Desarrollo</text>
+		<text class="con_rojo" x="100" y="170">de Sistemas de</text>
+		<text class="con_rojo" x="100" y="190">Información.</text>
+		<text class="con_rojo" x="100" y="235">----------------------------------</text>
+		<text class="con_rojo" x="135" y="255">2018 - 2020</text>
+		
+		<text class="tit_verde" x="100" y="70">Experiencia Laboral</text>
+		<text class="con_verde" x="100" y="100">----------------------------------</text>
+		<text class="con_verde" x="100" y="130">Analista de Desarrollo</text>
+		<text class="con_verde" x="100" y="150">de Software.</text>
+		<text class="con_verde" x="100" y="170"></text>
+		<text class="con_verde" x="100" y="190">Wiedii S.A.S.</text>
+		<text class="con_verde" x="100" y="235">----------------------------------</text>
+		<text class="con_verde" x="100" y="255">Marzo / Junio - 2021</text> 
+		
+		<text class="tit_azul" x="100" y="70">Experiencia Laboral</text>
+		<text class="con_azul" x="100" y="100">----------------------------------</text>
+		<text class="con_azul" x="100" y="130">Ventas de tarjetas de</text>
+		<text class="con_azul" x="100" y="150">crédito de Colpatria</text>
+		<text class="con_azul" x="100" y="170">por el CallCenter de</text>
+		<text class="con_azul" x="100" y="190">ASECOFIN S.A.S.</text>
+		<text class="con_azul" x="100" y="235">----------------------------------</text>
+		<text class="con_azul" x="135" y="255">2016 - 2018</text> 
+	
+		<text class="tit_amarillo" x="100" y="70">Contacto:</text>
+		<text class="con_amarillo" x="100" y="100">----------------------------------</text>
+		<text class="con_amarillo" x="100" y="130">Óscar González</text>
+		<text class="con_amarillo" x="100" y="150">(+57) 322 8858439</text>
+		<text class="con_amarillo" x="100" y="170">instagram:</text>
+		<text class="con_amarillo" x="100" y="190">@oigonzalezp</text>
+		<text class="con_amarillo" x="100" y="235">----------------------------------</text>
+		<text class="con_amarillo" x="135" y="255">2018 - 2021</text>
+		
+		<circle class="btn red" data-id="1" />
+		<circle class="btn green" data-id="2" />
+		<circle class="btn blue" data-id="3" />
+		<circle class="btn yellow" data-id="4" />
+		
+		</svg>`;
