c26d37a971b432a2186b9ee99caacd8320e41cc8
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-06-18 20:00:28 -0500

    Update main.js

--- autos/static/js/main.js
+++ autos/static/js/main.js
@@ -1,10 +1,9 @@
 generate_table(data);
-generate_tablew("../images/vista1.png", "ESTACAS");
-generate_tablew("../images/vista2.png", "FURGON");
-generate_tablew("../images/vista3.png", "NOSE");
-generate_tablew("../images/captura11a.png", "INTERIORES");
-generate_tablew("../images/captura11b.png", "INTERIORES");
-generate_tablew("../images/captura11d.png", "INTERIORES");
-generate_tablew("../images/captura10b.png", "MOTOR");
-generate_tablew("../images/captura19.png", "GARANTÍA");
-//generate_tablew("../images/captura17.png", "MARCA");
+generate_tablew("https://raw.githubusercontent.com/OscarGonzalezADSI/practica_inicial/master/autos/images/vista1.PNG", "ESTACAS");
+generate_tablew("https://raw.githubusercontent.com/OscarGonzalezADSI/practica_inicial/master/autos/images/vista2.png", "FURGON");
+generate_tablew("https://raw.githubusercontent.com/OscarGonzalezADSI/practica_inicial/master/autos/images/vista3.png", "NOSE");
+generate_tablew("https://raw.githubusercontent.com/OscarGonzalezADSI/practica_inicial/master/autos/images/captura11a.png", "INTERIORES");
+generate_tablew("https://raw.githubusercontent.com/OscarGonzalezADSI/practica_inicial/master/autos/images/captura11b.png", "INTERIORES");
+generate_tablew("https://raw.githubusercontent.com/OscarGonzalezADSI/practica_inicial/master/autos/images/captura11d.png", "INTERIORES");
+generate_tablew("https://raw.githubusercontent.com/OscarGonzalezADSI/practica_inicial/master/autos/images/captura10b.png", "MOTOR");
+generate_tablew("https://raw.githubusercontent.com/OscarGonzalezADSI/practica_inicial/master/autos/images/captura19.png", "GARANTÍA");
