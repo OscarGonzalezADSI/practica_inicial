1b4cfe09602087ed4409948ff53f9db37b6ae851
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-08-02 15:27:02 -0500

    Add files via upload

--- brokers/gestion/g_2022_07_29/entregables/software/index.php
+++ brokers/gestion/g_2022_07_29/entregables/software/index.php
@@ -32,8 +32,11 @@ if(isset($_SESSION["email"]) && isset($_SESSION["pass"]))
 	<body>
 	
 	<section id="sect1" class="sect">
-		<video src="https://rr4---sn-4g5lznlz.googlevideo.com/videoplayback?expire=1659136806&ei=xhbkYpurFdSm0_wPwOSWqA8&ip=216.131.74.162&id=o-ALigfv86cS58Q5Ymrwb_f21cDHPoOTE7Jp9nW99lDd_7&itag=137&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C278&source=youtube&requiressl=yes&spc=lT-KhqhNuiTS_RH-_zJwE65lJ5zUgl8&vprv=1&mime=video%2Fmp4&ns=PezhVaIOuWL9XLBgSxU-Q7cH&gir=yes&clen=31278428&otfp=1&dur=80.847&lmt=1607133029650157&keepalive=yes&fexp=24001373,24007246&c=WEB&txp=6216222&n=F-wjLzTSpUBxZA&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cspc%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cotfp%2Cdur%2Clmt&sig=AOq0QJ8wRAIgF4ACbU0-Tq4m36blpta45ahaKCfMucPSjLoJuxnCeUcCIE15580SdqRw8mYH5noyTUO0GTqES5K5v0kmaBNRShwM&redirect_counter=1&cm2rm=sn-5uae777s&req_id=d488bb5557da3ee&cms_redirect=yes&cmsv=e&mh=cC&mip=181.33.200.158&mm=34&mn=sn-4g5lznlz&ms=ltu&mt=1659115359&mv=u&mvi=4&pl=23&lsparams=mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhAPr46bJ2huUhwuifCOeeTzDug0oZAbb_DN3xvfuGSe3cAiAZrLdeayoTDaYVL2SZ0Bti5e2KISy5rCentbLDMbbTIA%3D%3D" autoplay="true" muted="true" loop="true" poster="https://carontestudio.com/img/contacto.jpg"></video>
-		<h1>Sección primera - Con vídeo</h1>
+		<video autoplay muted loop poster="https://carontestudio.com/img/contacto.jpg">
+		<source src="./video/video_bienvenida.mp4" type="video/mp4">
+		<source src="./video/video_bienvenida.mp4" type="video/mp4">
+		Your browser does not support the video tag.
+		</video>
 	</section>
 
 	</body>
@@ -101,7 +104,11 @@ if(isset($_SESSION["email"]) && isset($_SESSION["pass"]))
 	  </fieldset>
 	</form>
 	<section id="sect1" class="sect">
-		<video src="https://rr4---sn-4g5lznlz.googlevideo.com/videoplayback?expire=1659136806&ei=xhbkYpurFdSm0_wPwOSWqA8&ip=216.131.74.162&id=o-ALigfv86cS58Q5Ymrwb_f21cDHPoOTE7Jp9nW99lDd_7&itag=137&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C278&source=youtube&requiressl=yes&spc=lT-KhqhNuiTS_RH-_zJwE65lJ5zUgl8&vprv=1&mime=video%2Fmp4&ns=PezhVaIOuWL9XLBgSxU-Q7cH&gir=yes&clen=31278428&otfp=1&dur=80.847&lmt=1607133029650157&keepalive=yes&fexp=24001373,24007246&c=WEB&txp=6216222&n=F-wjLzTSpUBxZA&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cspc%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cotfp%2Cdur%2Clmt&sig=AOq0QJ8wRAIgF4ACbU0-Tq4m36blpta45ahaKCfMucPSjLoJuxnCeUcCIE15580SdqRw8mYH5noyTUO0GTqES5K5v0kmaBNRShwM&redirect_counter=1&cm2rm=sn-5uae777s&req_id=d488bb5557da3ee&cms_redirect=yes&cmsv=e&mh=cC&mip=181.33.200.158&mm=34&mn=sn-4g5lznlz&ms=ltu&mt=1659115359&mv=u&mvi=4&pl=23&lsparams=mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhAPr46bJ2huUhwuifCOeeTzDug0oZAbb_DN3xvfuGSe3cAiAZrLdeayoTDaYVL2SZ0Bti5e2KISy5rCentbLDMbbTIA%3D%3D" autoplay="true" muted="true" loop="true" poster="https://carontestudio.com/img/contacto.jpg"></video>
+		<video autoplay muted loop poster="https://carontestudio.com/img/contacto.jpg">
+			<source src="./video/video_bienvenida.mp4" type="video/mp4">
+			<source src="./video/video_bienvenida.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>
 	</section>
 	</body>
 	<script>
